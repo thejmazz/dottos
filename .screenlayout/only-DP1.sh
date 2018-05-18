3ed02c2902684547dbb0d78cfdb2039a58110063
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2018-05-18 10:06:43 -0400

    some arandr screenlayouts

---
+++ .screenlayout/only-DP1.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --primary --mode 2560x1600 --pos 0x0 --rotate normal --output DP2 --off --output HDMI2 --off --output HDMI1 --off --output VGA1 --off
---
+++ .screenlayout/work.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --mode 1440x900 --pos 0x700 --rotate normal --output DP1 --primary --mode 2560x1600 --pos 1440x0 --rotate normal --output DP2 --off --output HDMI2 --off --output HDMI1 --off --output VGA1 --off
