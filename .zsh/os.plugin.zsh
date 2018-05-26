972de0b2a14e551d9ce0cbde816d4d7a128e1978
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2018-05-26 18:15:56 -0400

    [zsh] update OpenStack plugin's 'ossh' to grab bastion IP itself

--- .zsh/os.plugin.zsh
+++ .zsh/os.plugin.zsh
@@ -19,24 +19,58 @@ function os_servers {
 }
 
 function ossh {
-    # assumes one subnet for private ips
+    # TODO cache these values ahead of time, or perhaps use an Ansible inventory
+    # TODO handle direct ssh w/o bastion
+
+    # TODO get bastion name somehow
+    bastion_name=bastion
+
+    # Assumes one subnet for private ips
     subnet_prefix=$(
         openstack subnet list -f json | \
         jq -r '.[].Subnet' | \
         sed -r s'/([0-9]+)\.([0-9]+)\.([0-9]+)\.[0-9]+\/24/\1\.\2\.\3/'
     )
 
+    # Assumes floating IP pool giving out IPs from same prefix as first in list
+    floating_ip_prefix=$(
+        openstack floating ip list -f value -c 'Floating IP Address' | \
+        head -n 1 | \
+        sed -r s'/([0-9]+)\.([0-9]+)\.([0-9]+)\.[0-9]+/\1\.\2\.\3/'
+    )
+
     server_ips=$(
         openstack server list -f json | \
         jq -r '.[] | select(.Name == "'$1'").Networks' | \
-        sed -r s'/[a-zA-Z0-9]+=//' | sed s'/, /\n/'
+        sed -r s'/[a-zA-Z0-9_]+=//' | sed s'/, /\n/'
     )
 
     private_ip=$(echo $server_ips | grep $subnet_prefix)
 
-    # TODO dont depend on rc-os-bastion ssh config
-    ssh ubuntu@$private_ip -o ProxyCommand='ssh rc-os-bastion -W %h:%p'
+    bastion_ips=$(
+        openstack server list -f json | \
+        jq -r '.[] | select(.Name == "'$bastion_name'").Networks' | \
+        sed -r s'/[a-zA-Z0-9_]+=//' | sed s'/, /\n/'
+    )
+
+    bastion_ip=$(echo $bastion_ips | grep $floating_ip_prefix)
+
+    # TODO use labels to determine core vs ubuntu, etc
+    ssh core@$private_ip -J core@$bastion_ip
 }
+
+alias ossafip="openstack server add floating ip"
+alias ossrfip="openstack server remove floating ip"
+alias ossasg="openstack server add security group"
+alias ossrsg="openstack server remove security group"
+alias osfl="openstack flavor list"
+alias ossl="openstack server list"
+alias ossc="openstack server create"
+alias ossd="openstack server delete"
+alias osil="openstack image list"
+alias osfipl="openstack floating ip list"
+alias ossgl="openstack security group list"
 
 compctl -K os_projects osp
 compctl -K os_servers ossh
+compctl -K os_servers ossd
